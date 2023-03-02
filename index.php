7c668ccc22a8d2b749e8c11378c63ad2fbb9e5b8
wmcharles <charleswafula73@gmail.com>
2023-03-02 17:29:31 +0300

    add index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>chatGPT</title>
+</head>
+
+<body>
+    <div class="content mt-4">
+        <h1>Testing ChatGPT</h1>
+        <table class="table" id="editable_table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">customerNumber</th>
+                    <th scope="col">customerName</th>
+                    <th scope="col">phone</th>
+                    <th scope="col">city</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- filled with jquery -->
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="./assets/js/script.js"></script>
+</body>
+
+</html>
